4d609d32966b4104a15fdd6de71557afd6e753b5
maxilvert <44968480+maxilvert@users.noreply.github.com>
2023-01-23 16:59:39 +0100

    init

--- Assets/Level/UI/ToggleCatalog.cs
+++ Assets/Level/UI/ToggleCatalog.cs
@@ -11,25 +11,27 @@ public class ToggleCatalog : MonoBehaviour
     private void Start()
     {
         catalog.SetActive(false);
-    
-    }
-   private void Awake()
-   {
-    toggleReference.action.started += Toggle;
-    
-   }
-    private void OnDestroy()
-   {
-    toggleReference.action.started -= Toggle;
-   }
-    private void Toggle(InputAction.CallbackContext context)
-   {
 
-    bool isActive = !catalog.activeSelf ;
-    catalog.SetActive(isActive);
-    catalog.transform.position = transform.forward + offset;
-    // catalog.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2);
-    catalog.transform.rotation = transform.rotation;
-    
-   }
+    }
+    private void Awake()
+    {
+        toggleReference.action.started += Toggle;
+
+    }
+    private void OnDestroy()
+    {
+        toggleReference.action.started -= Toggle;
+    }
+    private void Toggle(InputAction.CallbackContext context)
+    {
+        bool isActive = !catalog.activeSelf;
+        catalog.SetActive(isActive);
+
+        catalog.transform.position = transform.forward + offset;
+        // catalog.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2);
+        Quaternion lm = new Quaternion(0, transform.rotation.y - 180, 0, 0);
+        catalog.GetComponent<RectTransform>().rotation= new Quaternion(catalog.transform.eulerAngles.x, catalog.transform.eulerAngles.y + 180, catalog.transform.eulerAngles.z,0);
+        // catalog.transform.rotation = rotation;
+
+    }
 }
