2bc3bb5419658ae7014f496ed17d5e6f691a849b
maxilvert <44968480+maxilvert@users.noreply.github.com>
2023-01-25 14:35:13 +0100

    djkz

--- Assets/Level/UI/Catalog.cs
+++ Assets/Level/UI/Catalog.cs
@@ -3,6 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using System.Linq;
 
 public class Catalog : MonoBehaviour
 {
@@ -12,17 +13,28 @@ public class Catalog : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo("./Assets/Resources/");
+        // System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo("./Assets/Resources/");
     
-        var names  = dir.GetFiles("*.prefab");
-        for (var index = 0; index < names.Length; index++)
+        // var names  = dir.GetFiles("*.prefab");
+        // for (var index = 0; index < names.Length; index++)
+        // {
+        //     var nm = Path.GetFileNameWithoutExtension(names[index].Name);
+        //     Debug.Log(nm);
+        //     var temp = Instantiate(button, screen.transform);
+        //     var helper = temp.GetComponent<ButtonHelper>();
+        //     helper.text.text = nm;
+        //     helper.go = Resources.Load<GameObject>(nm);
+        // }
+
+        GameObject[] prefabs = Resources.LoadAll("", typeof(GameObject)).Cast<GameObject>().ToArray();
+        foreach (var item in prefabs)
         {
-            var nm = Path.GetFileNameWithoutExtension(names[index].Name);
-            Debug.Log(nm);
+            var nm = item.name;
             var temp = Instantiate(button, screen.transform);
             var helper = temp.GetComponent<ButtonHelper>();
             helper.text.text = nm;
-            helper.go = Resources.Load<GameObject>(nm);
+            helper.go = item;
         }
+
     }
 }
